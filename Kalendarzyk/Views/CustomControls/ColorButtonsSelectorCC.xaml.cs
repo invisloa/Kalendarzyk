e2e2e3b9e6549764d8621659e1a5cbed95d550c0
invisloa <91217120+invisloa@users.noreply.github.com>
2023-11-20 23:52:57 +0100

    CC for buttonColors

--- Kalendarzyk/Views/CustomControls/ColorButtonsSelectorCC.xaml.cs
+++ Kalendarzyk/Views/CustomControls/ColorButtonsSelectorCC.xaml.cs
@@ -1,9 +1,34 @@
+using System.Collections;
 using System.Windows.Input;
 
 namespace Kalendarzyk.Views.CustomControls;
 
 public partial class ColorButtonsSelectorCC : ContentView
 {
+	public static readonly BindableProperty ItemsSourceProperty = BindableProperty.Create(
+	nameof(ItemsSource),
+	typeof(IEnumerable),
+	typeof(ColorButtonsSelectorCC),
+	null);
+
+	// Bindable property for Command
+	public static readonly BindableProperty CommandProperty = BindableProperty.Create(
+		nameof(Command),
+		typeof(ICommand),
+		typeof(ColorButtonsSelectorCC),
+		null);
+
+	public IEnumerable ItemsSource
+	{
+		get => (IEnumerable)GetValue(ItemsSourceProperty);
+		set => SetValue(ItemsSourceProperty, value);
+	}
+
+	public ICommand Command
+	{
+		get => (ICommand)GetValue(CommandProperty);
+		set => SetValue(CommandProperty, value);
+	}
 	public static readonly BindableProperty SelectColorCommandProperty = BindableProperty.Create(
 		nameof(SelectColorCommand),
 		typeof(ICommand),
